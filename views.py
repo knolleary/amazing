a5651df7a10b685067c58b834eeae55805f6be67
Nicholas O'Leary <nick.oleary@gmail.com>
2013-06-18 22:19:30 +0000

    gzip content so nginx doesn't do it for me and strip the ETag

--- views.py
+++ views.py
@@ -1,14 +1,28 @@
+from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import hashlib
 from datetime import datetime
+import cStringIO,gzip
 
-def render(context):
-  response = render_to_response('amazing/index.html',context)
+def render(request,context):
+  t = loader.get_template('amazing/index.html')
+  c = RequestContext(request,context)
+  
+  zbuf = cStringIO.StringIO()
+  zfile = gzip.GzipFile(mode='wb',compresslevel=6,fileobj=zbuf)
+  zfile.write(t.render(c).encode('utf-8'))
+  zfile.close()
+  
+  cc = zbuf.getvalue()
+  response = HttpResponse(cc)
+  response['Content-Encoding'] = 'gzip'
+  response['Content-Length'] = str(len(cc))
+ 
   m = hashlib.md5()
   m.update(datetime.utcnow().isoformat())
-  response['etag'] = m.hexdigest()
+  response['ETag'] = '"'+m.hexdigest()+'"'
   return response
   
 @csrf_exempt
@@ -20,7 +34,7 @@ def validate_config(request):
   
 def sample(request):
   context = {"w":19,"h":25,"d":19}
-  return render(context);
+  return render(request,context);
   
 def edition(request):
   # default to large - as that is what existing subscribers will expect
@@ -33,5 +47,5 @@ def edition(request):
       elif request.GET["size"] == "large":
           context = {"w":19,"h":25,"d":19}
       
-  return render(context)
+  return render(request,context)
 
